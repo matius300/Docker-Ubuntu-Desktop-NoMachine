20a779982ce8dc74ae227cfe61f57f281811acaa
matius300 <33297541+matius300@users.noreply.github.com>
2022-03-09 02:03:01 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -6,7 +6,7 @@ stty susp undef
 clear
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
-echo "Downloading ngrok..."
+echo "SABAR ENTENI SEK YO KANG ..."
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
