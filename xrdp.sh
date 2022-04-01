dba72e1a8d9a4cc3efb81602938b0950440415f5
matius300 <33297541+matius300@users.noreply.github.com>
2022-04-02 04:44:25 +0700

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,11 +1,3 @@
-echo "PROGRESS TIME : 5 MNT"
-echo "USE STABIL CONNECTION"
-echo ""
-echo ""
-rm -fr google-xrdp.sh  ngrok  ngrok.zip
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok.zip > /dev/null 2>&1
-read -p "> Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 echo "====================="
